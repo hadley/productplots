7d6c6a1389f76c729e892b0eb506093eb8c456c4
hadley <h.wickham@gmail.com>
2009-11-26 09:23:54 -0600

    Break up monolithic test file into components

---
+++ tests/helper-division.r
@@ -0,0 +1,44 @@
+prop <- function(x) x / sum(x)
+make_df <- function(mat) {
+  rename(melt(mat), c("value" = ".wt"))
+}
+
+rand_array <- function(...) {
+  dims <- c(...)
+  array(runif(prod(dims)), dims)
+}
+
+#' Given a matrix of probabilities, compute the area that will be assigned to
+#' each combination, given the system of division.
+#'
+#' @param mat matrix or array of probabilities (or counts, to be standardised
+#'   to sum 1)
+#' @param divider list of divider functions
+calc_area <- function(mat, divider) {
+  df <- make_df(mat)
+  dims <- add_area(divide(df, divider = divider))
+  dims[c("level", names(df), "area")]
+}
+
+#' Standardise weight and area to sum to 1 within a level, and calulate ratio
+#' between the two.
+calc_ratio <- function(dims) {
+  ddply(dims, "level", transform, 
+    .wt = prop(.wt), 
+    area = prop(area), 
+    ratio = prop(area) / prop(.wt))
+}
+
+
+#' Expectation: output areas are proportional to input weights
+has_proportional_areas <- function() {
+  function(dims) {
+    ratios <- calc_ratio(dims)
+    incorrect <- subset(ratios, abs(ratio - 1) > 1e-6)  
+    
+    expectation(
+      nrow(incorrect) == 0,
+      paste(c("", capture.output(print(incorrect))), collapse = "\n")
+    )
+  }
+}
--- tests/test-division.r
+++ tests/test-division.r
@@ -1,48 +1,6 @@
 library(testthat)
 
-
-prop <- function(x) x / sum(x)
-make_df <- function(mat) {
-  rename(melt(mat), c("value" = ".wt"))
-}
-
-rand_array <- function(...) {
-  dims <- c(...)
-  array(runif(prod(dims)), dims)
-}
-
-#' Given a matrix of probabilities, compute the area that will be assigned to
-#' each combination, given the plot dividers.
-#'
-#' @param mat matrix or array of probabilities (or counts, to be standardised
-#'   to sum 1)
-#' @param divider list of divider functions
-calc_area <- function(mat, divider) {
-  df <- make_df(mat)
-  dims <- add_area(divide(df, divider = divider))
-  dims[c("level", names(df), "area")]
-}
-
-calc_ratio <- function(dims) {
-  ddply(dims, "level", transform, 
-    .wt = prop(.wt), 
-    area = prop(area), 
-    ratio = prop(area) / prop(.wt))
-}
-
-
-#' Expectation: output areas are proportional to input weights
-has_proportional_areas <- function() {
-  function(dims) {
-    ratios <- calc_ratio(dims)
-    incorrect <- subset(ratios, abs(ratio - 1) > 1e-6)  
-    
-    expectation(
-      nrow(incorrect) == 0,
-      str_join(c("", capture.output(print(incorrect))), collapse = "\n")
-    )
-  }
-}
+context("Division algorithm")
 
 test_that("2d areas are proportional to weights", {
   rand3x4 <- rand_array(3, 4)
