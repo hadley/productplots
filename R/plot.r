e9170014782b5fb65e7a3689a21257a23822d003
hadley <h.wickham@gmail.com>
2009-11-27 08:51:07 -0600

    Make border colour a parameter

--- R/plot.r
+++ R/plot.r
@@ -15,9 +15,9 @@ prodplot <- function(data, formula, divider = mosaic(), cascade = 0, scale_max =
   draw(res, ...)
 }
 
-draw <- function(df, alpha = 1) {
+draw <- function(df, alpha = 1, colour = "black") {
   ggplot(df, aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, order = level)) + 
-    geom_rect(colour = "black", alpha = alpha) +
+    geom_rect(colour = colour, alpha = alpha) +
     xlab(NULL) + ylab(NULL)
 }
 
