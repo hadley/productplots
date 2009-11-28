134742f2ad3ded6a09a6847694bcf20d0f0ee98d
hadley <h.wickham@gmail.com>
2009-11-28 09:30:52 -0600

    Treemap that works some of the time

--- R/partition-treemap.r
+++ R/partition-treemap.r
@@ -1,19 +1,24 @@
 # Adapated from SquarifiedLayout in 
 # http://www.cs.umd.edu/hcil/treemap-history/Treemaps-Java-Algorithms.zip
-treemap <- function(data, bounds) {
-  height <- bounds$ymax - bounds$ymin
-  width <- bounds$xmax - bounds$xmin
-
-  mid <- start
-  a <- data[1]
-  b <- 1
+treemap <- function(data, bounds, max = 1) {
+  if (length(data) == 0) return()
   
-  if (width < height) { # Tall and skinny
-    if (length(data) < 2) {
-      return(vspline(data, bounds))
+  h <- bounds$ymax - bounds$ymin
+  w <- bounds$xmax - bounds$xmin
+  x <- bounds$xmin
+  y <- bounds$ymin
+
+  mid <- 1
+  
+  a <- data[1]
+  b <- a
+  
+  if (w < h) { # Tall and skinny
+    if (length(data) <= 2) {
+      return(vspline(data, bounds, offset = 0))
     }
     
-    while (mid <= end) {
+    while (mid <= length(data)) {
       q <- data[mid]
       
       if (normAspect(h, w, a, b + q) > normAspect(h, w, a, b)) break
@@ -21,25 +26,26 @@ treemap <- function(data, bounds) {
       b <- b + q
     }
     
-    cbind(
-      vspline(data[start:mid], bounds(x, y, w, h * b)),
-      treemap(data[(mid + 1):end], bounds(x, y + h * b, w, h * (1 - b))))
+    rbind(
+      vspline(data[1:mid],    bound(y + h,     x + w, y + h * b, x), offset = 0),
+      treemap(data[-(1:mid)], bound(y + h * b, x + w, y,         x)))
       
   } else {  # Short and fat
-    if (length(data) < 2) {
-      return(hspline(data, bounds))
+    if (length(data) <= 2) {
+      return(hspline(data, bounds, offset = 0))
     }
 
-    while (mid <= end) {
+    while (mid <= length(data)) {
       q <- data[mid]
       if (normAspect(w, h, a, b + q) > normAspect(w, h, a, b)) break
       mid <- mid + 1
       b <- b + q
     }
     
-    cbind(
-      hspline(data[start:mid], bounds(x, y, w * b, h)),
-      treemap(data[(mid + 1):end], bounds(x + w * b, y, w * (1 - b), h)))
+    # browser()
+    rbind(
+      hspline(data[1:mid],    bound(y + h, x + w * b, y, x), offset = 0),
+      treemap(data[-(1:mid)], bound(y + h, x + w,     y, x + w * b)))
   }
 }
 
