dda4296cff9d6a8290b18d71bb0c9b5e3f0c62d3
hadley <h.wickham@gmail.com>
2009-12-04 17:45:18 -0600

    Tests for weighting

--- tests/test-plotting.r
+++ tests/test-plotting.r
@@ -2,7 +2,7 @@ library(testthat)
 
 context("Plot ui")
 
-test_that("formulas parsed correctly", {
+test_that("margins & conditions extracted", {
   
   expect_that(parse_product_formula(~ a)$marg, equals(c("a")))
   expect_that(parse_product_formula(~ a)$cond, equals(character()))
@@ -12,5 +12,21 @@ test_that("formulas parsed correctly", {
   expect_that(parse_product_formula(~ a | b + c + d)$cond, 
     equals(c("b", "c", "d")))
 
-  
+  expect_that(parse_product_formula(wt ~ a + c + d)$marg, 
+    equals(c("a", "c", "d")))
+  expect_that(parse_product_formula(wt ~ a | b + c + d)$cond, 
+    equals(c("b", "c", "d")))
+
+})
+
+test_that("dummy margin variable is ignored", {
+  expect_that(parse_product_formula(~ . | b + c + d)$cond, 
+    equals(c("b", "c", "d")))
+  expect_that(parse_product_formula(~ . | b + c + d)$marg, 
+    equals(character()))
+})
+
+test_that("weighting variable determined correctly", {
+  expect_that(parse_product_formula(wt ~ a)$wt, equals("wt"))
+  expect_that(parse_product_formula( ~ a)$wt, equals(NULL))
 })
