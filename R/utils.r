cff845bc615e575e641e3acf5c3b5e9fe93c4ba7
hadley <h.wickham@gmail.com>
2011-06-22 14:29:17 -0500

    Tidy file structure

--- R/utils.r
+++ R/utils.r
@@ -1,7 +1,3 @@
 prop <- function(x) x / sum(x, na.rm = TRUE)
 
 cv <- function(x, na.rm = FALSE) sd(x, na.rm = na.rm) / mean(x, na.rm = na.rm)
-
-rowProds <- function(x) apply(x, 1, prod)
-
-colProds <- function(x) apply(x, 2, prod)
