d2591d02d55681eae94f764ec551be7e4f933295
Michael Chirico <chiricom@google.com>
2022-12-16 16:43:01 -0800

    missed one

--- tests/testthat/test-plotting.r
+++ tests/testthat/test-plotting.r
@@ -1,28 +1,21 @@
 test_that("margins & conditions extracted", {
 
-  expect_that(parse_product_formula(~ a)$marg, testthat::equals(c("a")))
-  expect_that(parse_product_formula(~ a)$cond, testthat::equals(character()))
+  expect_identical(parse_product_formula(~ a)$marg, c("a"))
+  expect_identical(parse_product_formula(~ a)$cond, character())
 
-  expect_that(parse_product_formula(~ a + c + d)$marg,
-    testthat::equals(c("a", "c", "d")))
-  expect_that(parse_product_formula(~ a | b + c + d)$cond,
-    testthat::equals(c("b", "c", "d")))
+  expect_identical(parse_product_formula(~ a + c + d)$marg, c("a", "c", "d"))
+  expect_identical(parse_product_formula(~ a | b + c + d)$cond, c("b", "c", "d"))
 
-  expect_that(parse_product_formula(wt ~ a + c + d)$marg,
-    testthat::equals(c("a", "c", "d")))
-  expect_that(parse_product_formula(wt ~ a | b + c + d)$cond,
-    testthat::equals(c("b", "c", "d")))
-
+  expect_identical(parse_product_formula(wt ~ a + c + d)$marg, c("a", "c", "d"))
+  expect_identical(parse_product_formula(wt ~ a | b + c + d)$cond, c("b", "c", "d"))
 })
 
 test_that("dummy margin variable is ignored", {
-  expect_that(parse_product_formula(~ . | b + c + d)$cond,
-    testthat::equals(c("b", "c", "d")))
-  expect_that(parse_product_formula(~ . | b + c + d)$marg,
-    testthat::equals(character()))
+  expect_identical(parse_product_formula(~ . | b + c + d)$cond, c("b", "c", "d"))
+  expect_identical(parse_product_formula(~ . | b + c + d)$marg, character())
 })
 
 test_that("weighting variable determined correctly", {
-  expect_that(parse_product_formula(wt ~ a)$wt, testthat::equals("wt"))
-  expect_that(parse_product_formula( ~ a)$wt, testthat::equals(character()))
+  expect_identical(parse_product_formula(wt ~ a)$wt, "wt")
+  expect_identical(parse_product_formula( ~ a)$wt, character())
 })
